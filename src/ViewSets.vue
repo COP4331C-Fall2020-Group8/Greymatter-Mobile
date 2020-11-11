f6ff59f1f70df22f35c55ec4115f702b01e99ca2
Wayne He <wayneh000@knights.ucf.edu>
2020-11-10 20:58:29 -0500

    Added back button. Sets are displayed as buttons.

--- src/ViewSets.vue
+++ src/ViewSets.vue
@@ -1,6 +1,25 @@
 <template>
     <view class="container">
-
+        <text class="pageHeader">View your sets!</text>
+        <text class="label">Search Sets</text>
+        <TextInput class="textFilter" v-model="textfilter" hint="Fiter set name here" />
+        <ScrollView>
+            <button
+                class="set"
+                title="Sample Set"
+                @press="viewSet"
+            />
+            <button
+                class="set"
+                title="Sample Set 2"
+                @press="viewSet"
+            />
+        </ScrollView>
+        <button
+            class="backBtn"
+            title="Go Back"
+            @press="goBack"
+        />
     </view>
 </template>
 
@@ -8,6 +27,12 @@
 import statusbar from "./components/statusbar.vue";
 
 export default {
+    data() {
+        return {
+            textfilter: ''
+        }
+    },
+
     components: {
         statusbar
     },
@@ -16,10 +41,45 @@ export default {
         navigation: {
             type: Object
         }
+    },
+
+    methods: {
+        goBack() {
+            this.navigation.goBack();
+        },
+        viewSet() {
+            alert("Under construction");
+        }
     }
 }
 </script>
 
 <style>
+.container {
+    background-color: grey;
+    flex-direction: column;
+    flex: 1;
+}
 
+.label {
+    margin-left: 10px;
+}
+
+.pageHeader {
+    font-size: 40px;
+    text-align: center;
+}
+
+.set {
+    padding: 10px;
+    width: 300px;
+    height: 100px;
+}
+
+.textFilter {
+    background-color: white;
+    width: auto;
+    margin-left: 10px;
+    margin-right: 10px;
+}
 </style>
